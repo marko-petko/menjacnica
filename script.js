adff6d83d125b503958bfe3e4649f93bb93ce75a
marko-petko <marko.petko@limolabs.com>
2020-11-27 19:06:06 +0100

    update project version-1.1

--- script.js
+++ script.js
@@ -1,115 +1,62 @@
-const rsd = document.getElementById('dinar');
-const eur = document.getElementById('euro');
-const usd = document.getElementById('dolar');
-const chf = document.getElementById('franak');
+const inputDinar = document.getElementById('dinar');
+const inputEuro = document.getElementById('euro');
+const inputDolar = document.getElementById('dolar');
+const inputFranak = document.getElementById('franak');
 
-rsd.addEventListener('input', dinar);
+let mydata = JSON.parse(data);
+let rsd = 1;
+let eur = mydata[0]["SREDNJI KURS"];
+let usd = mydata[15]["SREDNJI KURS"];
+let chf = mydata[13]["SREDNJI KURS"];
+
+inputDinar.addEventListener('input', dinar);
 
 function dinar() {
-    const dinarRSD = parseInt(rsd.value);
-    const euroE = 0.0085 * dinarRSD;
-    const usdD = 0.010 * dinarRSD;
-    const chfSwiss = 0.0092 * dinarRSD;
-    eur.value = euroE;
-    usd.value = usdD;
-    chf.value = chfSwiss;
+    const euroE = rsd/eur;
+    const usdD = rsd/usd;
+    const chfSwiss = rsd/chf;
+    inputEuro.value = euroE.toFixed(4);
+    inputDolar.value = usdD.toFixed(4);
+    inputFranak.value = chfSwiss.toFixed(4);
 }
 
-eur.addEventListener('input', euro);
+inputEuro.addEventListener('input', euro);
 
 function euro() {
-    const euroE = parseInt(eur.value);
-    const dinarRSD = 117.5701 * euroE;
-    const usdD = 1.18 * euroE;
-    const chfSwiss = 1.08 * euroE;
-    rsd.value = dinarRSD;
-    usd.value = usdD;
-    chf.value = chfSwiss;
+    const dinarRSD = eur/rsd;
+    const usdD = eur/usd;
+    const chfSwiss = eur/chf;
+    inputDinar.value = dinarRSD.toFixed(4);
+    inputDolar.value = usdD.toFixed(4);
+    inputFranak.value = chfSwiss.toFixed(4);
 }
 
-usd.addEventListener('input', dolar);
+inputDolar.addEventListener('input', dolar);
 
 function dolar() {
-    const dollar = parseInt(usd.value);
-    const dinarRSD = 99.36 * dollar;
-    const euroE = 0.85 * dollar;
-    const chfSwiss = 0.91 * dollar;
-    rsd.value = dinarRSD;
-    eur.value = euroE;
-    chf.value = chfSwiss;
+    const dinarRSD = usd/rsd;
+    const euroE = usd/eur;
+    const chfSwiss = usd/chf;
+    inputDinar.value = dinarRSD.toFixed(4);
+    inputEuro.value = euroE.toFixed(4);
+    inputFranak.value = chfSwiss.toFixed(4);
 }
 
-chf.addEventListener('input', chfS);
+inputFranak.addEventListener('input', franak);
 
-function chfS() {
-    const chfSwiss = parseInt(chf.value);
-    const dinarRSD = 108.78 * chfSwiss;
-    const euroE = 0.93 * chfSwiss;
-    const usdD = 1.10 * chfSwiss;
-    rsd.value = dinarRSD;
-    eur.value = euroE;
-    usd.value = usdD;
+function franak() {
+    const dinarRSD = chf/rsd;
+    const euroE = chf/eur;
+    const usdD = chf/usd;
+    inputDinar.value = dinarRSD.toFixed(4);
+    inputEuro.value = euroE.toFixed(4);
+    inputDolar.value = usdD.toFixed(4);
 }
 
-var api = "https://kurs.resenje.org/api/v1/currencies/eur/rates/today";
-$('#view-doctors').on('click',function() {      
-   var button = $(this);      
-   $.ajax({
-      url:api,
-      method:'GET',
-      cache:false,
-      type:"text/json"
-   })
-   .always(function(){
-      $(button).html('Load Doctor Data...');
-   })
-   .done(function(evt) {
-      // Disable button
-      $(button).prop('disabled',true);
-      // Set timeout for lazy loading
-      setTimeout(function(){
-         var result = JSON.parse(evt);
-         var html = '<h2>Data Dokter</h2>';
-         html += '<div class="tables-doctor-content">';
-         for(var i=0;i < result.Data.length; i++) {
-            html +='<h3 class="tables-doctor-name">'+result.Data[i].DoctorName+'</h3>'
-               +'<p class="tables-doctor-title">'+result.Data[i].Specialist+'</p>';
-
-            if(result.Data[i].Hospitals.length > 0) {  
-               html +='<table class="table">'
-                  +'<thead>'
-                  +'<tr>'
-                  +'<th scope="col">Nama Rumah Sakit</th>'
-                  +'<th scope="col">Alamat</th>'
-                  +'<th scope="col">Jadwal Praktek</th>'
-                  +'</tr>'
-                  +'</thead>'
-                  +'<tbody>';
-
-               for(var j=0;j < result.Data[i].Hospitals.length; j++) {
-                  html +='<tr>'
-                     +'<th scope="row">'+result.Data[i].Hospitals[j].Name+'</th>'
-                     +'<td>'+result.Data[i].Hospitals[j].Address+'</td>'
-                     +'<td>'+result.Data[i].Hospitals[j].Schedule+'</td>'
-                     +'</tr>';
-               }
-               html +='</tbody></table>';
-            }
-         }
-          html +='</div>';
-
-         // Set all content
-         $('.tables-doctor').html(html);
-
-      },1000); 
-   })
-   .fail(function() {
-      alert('Error : Failed to reach API Url or check your connection');
-      $(button).prop('disabled',false);
-   })
-   .then(function(evt){
-      setTimeout(function(){        
-         $(button).css({'background-color':'#ccc'}).hide();          
-      },1000);
-   });
+$(document).ready(function() {
+    let mydata = JSON.parse(data);
+    console.log(mydata);
 });
+
+
+
