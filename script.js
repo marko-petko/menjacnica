078531f50e4e85ff5ec46b7d5e88775bdee27178
marko-petko <marko.petko@limolabs.com>
2020-11-30 13:40:37 +0100

    update project version-1.2

--- script.js
+++ script.js
@@ -12,45 +12,48 @@ let chf = mydata[13]["SREDNJI KURS"];
 inputDinar.addEventListener('input', dinar);
 
 function dinar() {
-    const euroE = rsd/eur;
-    const usdD = rsd/usd;
-    const chfSwiss = rsd/chf;
-    inputEuro.value = euroE.toFixed(4);
-    inputDolar.value = usdD.toFixed(4);
-    inputFranak.value = chfSwiss.toFixed(4);
+    const dinarRSD = parseInt(inputDinar.value);
+    const euroE = dinarRSD/eur;
+    const usdD = dinarRSD/usd;
+    const chfSwiss = dinarRSD/chf;
+    setOutput(dinarRSD, euroE, usdD, chfSwiss);
 }
 
 inputEuro.addEventListener('input', euro);
 
 function euro() {
-    const dinarRSD = eur/rsd;
-    const usdD = eur/usd;
-    const chfSwiss = eur/chf;
-    inputDinar.value = dinarRSD.toFixed(4);
-    inputDolar.value = usdD.toFixed(4);
-    inputFranak.value = chfSwiss.toFixed(4);
+    const euroE = parseInt(inputEuro.value);
+    const dinarRSD = euroE/rsd;    
+    const usdD = euroE/usd;
+    const chfSwiss = euroE/chf;
+    setOutput(dinarRSD, euroE, usdD, chfSwiss);
 }
 
 inputDolar.addEventListener('input', dolar);
 
 function dolar() {
-    const dinarRSD = usd/rsd;
-    const euroE = usd/eur;
-    const chfSwiss = usd/chf;
-    inputDinar.value = dinarRSD.toFixed(4);
-    inputEuro.value = euroE.toFixed(4);
-    inputFranak.value = chfSwiss.toFixed(4);
+    const usdD = parseInt(inputDolar.value);
+    const dinarRSD = usdD/rsd;
+    const euroE = usdD/eur;    
+    const chfSwiss = usdD/chf;
+    setOutput(dinarRSD, euroE, usdD, chfSwiss);
 }
 
 inputFranak.addEventListener('input', franak);
 
 function franak() {
-    const dinarRSD = chf/rsd;
-    const euroE = chf/eur;
-    const usdD = chf/usd;
-    inputDinar.value = dinarRSD.toFixed(4);
-    inputEuro.value = euroE.toFixed(4);
-    inputDolar.value = usdD.toFixed(4);
+    const chfSwiss = parseInt(inputFranak.value);
+    const dinarRSD = chfSwiss/rsd;
+    const euroE = chfSwiss/eur;
+    const usdD = chfSwiss/usd;    
+    setOutput(dinarRSD, euroE, usdD, chfSwiss);
+}
+
+function setOutput(rsd, eur, usd, chf) {
+    inputDinar.value = rsd;
+    inputEuro.value = eur.toFixed(4);
+    inputDolar.value = usd.toFixed(4);
+    inputFranak.value = chf.toFixed(4);
 }
 
 $(document).ready(function() {
